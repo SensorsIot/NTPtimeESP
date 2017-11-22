2f77a78d574dfe90d07d943676887a97041966e7
Michael Dombrowski <mikepi3.14@gmail.com>
2017-11-22 14:04:59 -0500

    Added epoch time and fixed types for daylight saving time parameters
    Also fixed parameter explanation in example

--- NTPtimeESP.h
+++ NTPtimeESP.h
@@ -25,13 +25,14 @@ struct strDateTime
   byte month;
   byte day;
   byte dayofWeek;
+  unsigned long epochTime;
   boolean valid;
 };
 
 class NTPtime {
   public:
     NTPtime(String NTPtime);
-    strDateTime getNTPtime(float _timeZone, boolean _DayLightSaving);
+    strDateTime getNTPtime(float _timeZone, int _DayLightSaving);
     void printDateTime(strDateTime _dateTime);
     bool setSendInterval(unsigned long _sendInterval);  // in seconds
     bool setRecvTimeout(unsigned long _recvTimeout);    // in seconds
@@ -45,7 +46,7 @@ class NTPtime {
     strDateTime ConvertUnixTimestamp( unsigned long _tempTimeStamp);
     boolean summerTime(unsigned long _timeStamp );
     boolean daylightSavingTime(unsigned long _timeStamp);
-    unsigned long adjustTimeZone(unsigned long _timeStamp, float _timeZone, byte _DayLightSavingSaving);
+    unsigned long adjustTimeZone(unsigned long _timeStamp, float _timeZone, int _DayLightSavingSaving);
     WiFiUDP UDPNTPClient;
 };
 #endif
--- examples/NTPtimeESP/NTPtimeESP.ino
+++ examples/NTPtimeESP/NTPtimeESP.ino
@@ -48,7 +48,7 @@ void setup() {
 
 void loop() {
 
-  // first parameter: Time zone in floating point (for India); second parameter: 1 for European summer time; 2 for US daylight saving time (contributed by viewwer, not tested by me)
+  // first parameter: Time zone in floating point (for India); second parameter: 1 for European summer time; 2 for US daylight saving time; 0 for no DST adjustment; (contributed by viewwer, not tested by me)
   dateTime = NTPch.getNTPtime(1.0, 1);
 
   // check dateTime.valid before using the returned time
